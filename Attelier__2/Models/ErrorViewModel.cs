ff97af0f793ca2cea4534cfb32dc9c066b3b911a
OuchayanRim <rimouchayan@gmail.com>
2025-03-22 11:52:13 +0000

    Ajoutez des fichiers projet.

---
+++ Attelier__2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Attelier__2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
